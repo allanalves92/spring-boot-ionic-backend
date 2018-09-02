5085685d76cf331770276b6bc857cbb1a28e8a13
Allan Alves <allanalvesmail@gmail.com>
2018-09-02 10:53:11 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/allanalves/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/allanalves/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.allanalves.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.allanalves.cursomc.domain.Categoria;
 import com.allanalves.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias,
+			Pageable pageRequest);
 
 }
