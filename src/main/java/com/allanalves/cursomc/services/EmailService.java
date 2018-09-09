5263f3c9d288fd16e834008dfea9e3d5b5af9d69
Allan Alves <allanalvesmail@gmail.com>
2018-09-09 11:58:32 -0300

    Implementação de envio de e-mail html com thymeleaf

--- src/main/java/com/allanalves/cursomc/services/EmailService.java
+++ src/main/java/com/allanalves/cursomc/services/EmailService.java
@@ -1,13 +1,18 @@
 package com.allanalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.allanalves.cursomc.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido pedido);
-	
+
 	void sendEmail(SimpleMailMessage msg);
 
+	void sendOrderConfirmationHtmlEmail(Pedido pedido);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
