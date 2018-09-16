d4bcacafb0d077f1193d5cbe023523daa6b90832
Allan Alves <allanalvesmail@gmail.com>
2018-09-16 14:23:05 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/allanalves/cursomc/domain/Cliente.java
+++ src/main/java/com/allanalves/cursomc/domain/Cliente.java
@@ -55,6 +55,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "PERFIS")
 	private Set<Integer> perfis = new HashSet<>();
 
+	private String imageUrl;
+
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -150,6 +152,14 @@ public class Cliente implements Serializable {
 		perfis.add(perfil.getCod());
 	}
 
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -174,5 +184,4 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-
 }
