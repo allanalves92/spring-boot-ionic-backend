1fe58069aec463547d4658c3c8e86b268c7dd68d
Allan Alves <allanalvesmail@gmail.com>
2018-09-16 17:03:28 -0300

    Padronizando formato das exceções

--- src/main/java/com/allanalves/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/allanalves/cursomc/repositories/CidadeRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.allanalves.cursomc.domain.Cidade;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
 
+	@Transactional(readOnly = true)
 	List<Cidade> findByEstadoId(Integer estadoId);
 
 }
