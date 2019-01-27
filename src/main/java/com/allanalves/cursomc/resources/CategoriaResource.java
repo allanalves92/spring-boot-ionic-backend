79e42b4c18f73bb6936b556e2a2512232badc79c
Allan Alves <allanalvesmail@gmail.com>
2019-01-27 14:07:08 -0200

    Documentação com Swagger - Parte 2

--- src/main/java/com/allanalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/allanalves/cursomc/resources/CategoriaResource.java
@@ -25,6 +25,8 @@ import com.allanalves.cursomc.domain.Categoria;
 import com.allanalves.cursomc.dto.CategoriaDTO;
 import com.allanalves.cursomc.services.CategoriaService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
@@ -32,6 +34,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 
+	@ApiOperation(value="Busca categoria por id")
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria categoria = service.find(id);
@@ -40,6 +43,7 @@ public class CategoriaResource {
 
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
+	@ApiOperation(value="Insere categoria")
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO) {
 		Categoria categoria = service.fromDTO(categoriaDTO);
 		categoria = service.insert(categoria);
@@ -50,6 +54,7 @@ public class CategoriaResource {
 
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PutMapping(value = "/{id}")
+	@ApiOperation(value="Atualiza categoria por id")
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO categoriaDTO, @PathVariable Integer id) {
 		Categoria categoria = service.fromDTO(categoriaDTO);
 		categoria.setId(id);
@@ -59,12 +64,14 @@ public class CategoriaResource {
 
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@DeleteMapping(value = "/{id}")
+	@ApiOperation(value="Exclui categoria por id")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
 	@GetMapping
+	@ApiOperation(value="Recupera todas as categorias")
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> categorias = service.findAll();
 		List<CategoriaDTO> listDto = categorias.stream().map(categoria -> new CategoriaDTO(categoria))
@@ -73,6 +80,7 @@ public class CategoriaResource {
 	}
 
 	@GetMapping(value = "/page")
+	@ApiOperation(value="Recupera todas as categorias com paginação")
 	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
 			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
